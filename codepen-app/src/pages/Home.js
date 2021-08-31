4ab9209011d14005965309d3e3ac480cbf67fa5d
Galip <galipsvt16@gmail.com>
2021-08-31 12:09:16 +0200

    home

--- codepen-app/src/pages/Home.js
+++ codepen-app/src/pages/Home.js
@@ -15,7 +15,35 @@ function Home() {
                 </p>
                 <img src="/codepen.png" alt="codepen-screenshot" />
                 <a href="https://codepen.io/">Link to Codepen</a>
+                <hr />
+                <h1>Codepen</h1>
+                <p>
+                    CodePen is a social development environment. At its heart, it allows
+                    you to write code in the browser, and see the results of it as you
+                    build. A useful and liberating online code editor for developers of
+                    any skill, and particularly empowering for people learning to code. We
+                    focus primarily on front-end languages like HTML, CSS, JavaScript, and
+                    preprocessing syntaxes that turn into those things.
+                </p>
+                <img src="/codepen.png" alt="codepen-screenshot" />
+                <a href="https://codepen.io/">Link to Codepen</a>
+                <hr />
+                <h1>Codepen</h1>
+                <p>
+                    CodePen is a social development environment. At its heart, it allows
+                    you to write code in the browser, and see the results of it as you
+                    build. A useful and liberating online code editor for developers of
+                    any skill, and particularly empowering for people learning to code. We
+                    focus primarily on front-end languages like HTML, CSS, JavaScript, and
+                    preprocessing syntaxes that turn into those things.
+                </p>
+                <img src="/codepen.png" alt="codepen-screenshot" />
+                <a href="https://codepen.io/">Link to Codepen</a>
             </main>
+
+            <div className="urlserach">
+                <input type="text" placeholder="cafer" />
+            </div>
         </div>
     )
 }
