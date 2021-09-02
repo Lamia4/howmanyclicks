d3132b9c081b46d8abdd4ff6e570a73977363ea0
Galip <galipsvt16@gmail.com>
2021-09-02 12:01:46 +0200

    alles

--- codepen-app/backend/server.js
+++ codepen-app/backend/server.js
@@ -1,18 +1,12 @@
 import express from "express";
 import dotenv from "dotenv";
 import database from "./lib/database.js";
+import cors from "cors"
+dotenv.config();
 
-
-<<<<<<< HEAD
 const server = express();
 
-var cors = require('cors')
-
 server.use(cors())
-
-=======
-dotenv.config();
->>>>>>> fbdb06bbf923b025758aa740ddd93aba68f1f996
 
 database.init();
 
@@ -22,7 +16,7 @@ server.listen(process.env.PORT, () => {
 })
 
 server.use(express.json());
-server.use(express.urlencoded({extended: true}));
+server.use(express.urlencoded({ extended: true }));
 
 server.get("/links", (req, res) => {
     let linksInfoLocalStorage = `[{"title":"CodePen","text":"Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit, nihil.","imgUrl":"https://i.ytimg.com/vi/H46C8rEaVHg/maxresdefault.jpg","link":"https://codepen.io/trending"},{"title":"Youtube","text":"Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit, nihil.","imgUrl":"https://www.sozunuz.com/wp-content/uploads/2020/11/Youtube-%C3%87%C3%B6kt%C3%BC.jpg","link":"https://www.youtube.com/"},{"title":"Amazon","text":"Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit, nihil.","imgUrl":"https://www.schweitzer-projects.de/images/Inhalte/aktuell/amazon-de.jpg","link":"https://www.amazon.de/"}]`
