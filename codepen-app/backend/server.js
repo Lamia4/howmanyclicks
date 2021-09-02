3e0f3c444b272bec0ae5db14c404627be3d63235
Galip <galipsvt16@gmail.com>
2021-09-02 11:55:57 +0200

    done

--- codepen-app/backend/server.js
+++ codepen-app/backend/server.js
@@ -3,12 +3,17 @@ const express = require("express");
 const dotenv = require("dotenv");
 dotenv.config();
 
-
-
 const server = express();
 
+var cors = require('cors')
+
+server.use(cors())
+
+
+
+
 server.listen(process.env.PORT, () => {
-    console.log("Server listening on Port 3434");
+    console.log("Server listening on Port 3435");
 })
 
 
@@ -18,3 +23,5 @@ server.get("/links", (req, res) => {
     let linksInfoLocalStorage = `[{"title":"CodePen","text":"Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit, nihil.","imgUrl":"https://i.ytimg.com/vi/H46C8rEaVHg/maxresdefault.jpg","link":"https://codepen.io/trending"},{"title":"Youtube","text":"Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit, nihil.","imgUrl":"https://www.sozunuz.com/wp-content/uploads/2020/11/Youtube-%C3%87%C3%B6kt%C3%BC.jpg","link":"https://www.youtube.com/"},{"title":"Amazon","text":"Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit, nihil.","imgUrl":"https://www.schweitzer-projects.de/images/Inhalte/aktuell/amazon-de.jpg","link":"https://www.amazon.de/"}]`
     res.send(linksInfoLocalStorage);
 })
+
+
